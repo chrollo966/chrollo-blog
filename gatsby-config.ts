82c798e8e3596ad648ad080f0e8ee25c9363c6bf
chrollo966 <rhinocerossutra@gmail.com>
2022-12-29 21:32:38 +0900

    image plugin introduced

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -3,7 +3,7 @@ import type { GatsbyConfig } from "gatsby"
 const config: GatsbyConfig = {
   siteMetadata: {
     title: `Chrollo Blog`,
-    siteUrl: `https://www.yourdomain.tld`,
+    siteUrl: `https://chrolloblogmain.gatsbyjs.io/`,
   },
   // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
   // If you use VSCode you can also use the GraphQL plugin
@@ -12,6 +12,8 @@ const config: GatsbyConfig = {
   plugins: [
     `gatsby-plugin-sass`,
     `gatsby-plugin-vanilla-extract`,
+    `gatsby-plugin-image`,
+    `gatsby-plugin-sharp`,
   ],
 }
 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,11 +3,16 @@ import { Link } from 'gatsby'
 import type { HeadFC, PageProps } from "gatsby"
 
 import Layout from '../components/layout'
+import { StaticImage } from 'gatsby-plugin-image'
 
 const IndexPage: React.FC<PageProps> = () => {
   return (
     <Layout pageTitle='Home Page'>
       <p>I'm making this by following the Gatsby Tutorial.</p>
+      <StaticImage
+        alt='A dog'
+        src='https://plus.unsplash.com/premium_photo-1668208365386-4198381c6f6e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2787&q=80'
+      />
     </Layout>
   )
 }
